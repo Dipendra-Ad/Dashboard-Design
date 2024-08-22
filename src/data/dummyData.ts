65c35bd8521788976af056c488a5bbb9b178306e
Dipendra-Ad <adhikari.dipendra@varicon.com.au>
2024-08-22 23:01:51 +0545

    makes modification

--- src/data/dummyData.ts
+++ src/data/dummyData.ts
@@ -10,8 +10,6 @@ export interface User {
   export const dummyUsers: User[] = [
     { id: 1, username: 'john_doe', password: 'password123', email: 'john@example.com' },
     { id: 2, username: 'jane_smith', password: 'password456', email: 'jane@example.com' },
-    { id: 1, username: 'john_doe', password: 'password123', email: 'john@example.com' },
-    { id: 2, username: 'jane_smith', password: 'password456', email: 'jane@example.com' },
   ];
   
   
