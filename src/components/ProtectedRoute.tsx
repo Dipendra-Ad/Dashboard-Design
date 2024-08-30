967b791fb74fcf5bb3b0d770ee7670d7a3a3ed34
Dipendra-Ad <adhikari.dipendra@varicon.com.au>
2024-08-30 19:57:35 +0545

    login and authcontext changed

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,19 +1,3 @@
-// import React from "react";
-// import { Navigate } from "react-router-dom";
-// import { useAuth } from "../context/AuthContext";
-
-// interface ProtectedRouteProps {
-//   element: JSX.Element;
-// }
-
-// const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
-//   const { isAuthenticated } = useAuth();
-
-//   return isAuthenticated ? element : <Navigate to="/login" />;
-// };
-
-// export default ProtectedRoute;
-
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
